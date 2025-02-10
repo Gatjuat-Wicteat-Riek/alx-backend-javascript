084b35255133264cb2e85bb391a833a6feb9a69b
Gatjuat-Wicteat-Riek <gatjuatriek@gmail.com>
2025-02-10 22:04:51 +0000

    Node js basics

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,10 @@
 /**
- * Prints the given message to STDOUT.
- *
- * @param {msg} str - The message to be printed.
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
-function displayMessage(msg) {
+const displayMessage = (msg) => {
   console.log(msg);
-}
+};
 
 module.exports = displayMessage;
